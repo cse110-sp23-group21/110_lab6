42165fb4db74cb708f33aee37df472a6abab33d2
vanvansan <103155715+vanvansan@users.noreply.github.com>
2023-05-12 16:35:03 -0700

    A1-A7

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -7,10 +7,18 @@ class RecipeCard extends HTMLElement {
     super(); // Inheret everything from HTMLElement
 
     // EXPOSE - START (All expose numbers start with A)
+    const shadow = elementRef.attachShadow({ mode: "open" });
     // A1. TODO - Attach the shadow DOM to this Web Component (leave the mode open)
+    const article = document.createElement("article");
     // A2. TODO - Create an <article> element - This will hold our markup once our data is set
+    const style = document.createElement("style");
     // A3. TODO - Create a style element - This will hold all of the styles for the Web Component
+    const styleSource = document.querySelector('style');
+    style.textContent = styleSource.textContent;
     // A4. TODO - Insert all of the styles from cardTemplate.html into the <style> element you just made
+    shadow.appendChild(style);
+    shadow.appendChild(article);
+
     // A5. TODO - Append the <style> and <article> elements to the Shadow DOM
   }
 
@@ -40,12 +48,27 @@ class RecipeCard extends HTMLElement {
     if (!data) return;
 
     // A6. TODO - Select the <article> we added to the Shadow DOM in the constructor
+
+    const article = this.shadow.querySelector("article");
+
+
     // A7. TODO - Set the contents of the <article> with the <article> template given in
     //           cardTemplate.html and the data passed in (You should only have one <article>,
     //           do not nest an <article> inside another <article>). You should use Template
     //           literals (tempalte strings) and element.innerHTML for this.
+
+    article = document.querySelector('article');
   }
 }
 
 // A8. TODO - Define the Class as a customElement so that you can create
 //           'recipe-card' elements
+
+class customElement extends HTMLParagraphElement {
+  constructor() {
+    // Always call super first in constructor
+    super();
+
+    // Element functionality written in here
+  }
+}
